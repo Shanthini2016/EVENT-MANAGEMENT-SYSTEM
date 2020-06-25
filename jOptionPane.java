cfe86e056fc35e8daa6193e72bceb9747029dccf
Shanthini2016 <57266597+Shanthini2016@users.noreply.github.com>
2020-06-25 14:36:23 +0530

    Add files via upload

---
+++ jOptionPane.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Restaurant_s;
+
+import javax.swing.JRootPane;
+
+/**
+ *
+ * @author LOGANATHAN
+ */
+class jOptionPane {
+
+    static void showMessageDialog(JRootPane rootPane, String invalid_Usernamee_and_Password_) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
